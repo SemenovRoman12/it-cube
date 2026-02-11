9607a12e8489ce9560d311ed41883c128f61a163
pynhaXD <roma151205@mail.ru>
2026-02-11 12:03:11 +0700

    Create api token

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import {API_URL} from './core/http/api-url.token';
+import {environment} from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    {
+      provide: API_URL,
+      useValue: environment.api_url,
+    }
   ]
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  api_url: "https://92692bcec18200f1.mokky.dev"
+};
