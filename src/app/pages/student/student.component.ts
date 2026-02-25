fd840835f412ea83da183aed802903fb11bc89c4
pynhaXD <roma151205@mail.ru>
2026-02-26 01:27:38 +0700

    add role guards

---
+++ src/app/pages/student/student.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'student',
+  imports: [],
+  templateUrl: './student.component.html',
+  styleUrl: './student.component.scss',
+})
+export class StudentComponent {
+
+}
