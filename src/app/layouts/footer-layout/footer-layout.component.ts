c586d52db1e71ed502cd8f9858e0cd509ca86e4f
pynhaXD <roma151205@mail.ru>
2026-02-28 21:59:30 +0700

    footer/header

--- src/app/layouts/footer-layout/footer-layout.component.ts
+++ src/app/layouts/footer-layout/footer-layout.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'footer-layout',
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './footer-layout.component.html',
   styleUrl: './footer-layout.component.scss',
 })
