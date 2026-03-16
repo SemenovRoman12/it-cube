4a4164bf4cf02352152684115ecc4921dd9a01af
pynhaXD <roma151205@mail.ru>
2026-03-16 15:13:54 +0700

    changer theme

--- src/app/layouts/header-layout/header-layout.component.ts
+++ src/app/layouts/header-layout/header-layout.component.ts
@@ -1,13 +1,19 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {Component, ChangeDetectionStrategy, signal, effect} from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'header-layout',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatIconButton, MatIcon],
   templateUrl: './header-layout.component.html',
   styleUrl: './header-layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderLayoutComponent {
+  public darkMode = signal<boolean>(false);
 
+  public setDarkMode = effect(() => {
+    document.documentElement.classList.toggle('dark', this.darkMode());
+  });
 }
