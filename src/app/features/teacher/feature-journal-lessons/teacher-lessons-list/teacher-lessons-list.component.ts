8efa436f6cf362a52ab2329905fe88215486190a
pynhaXD <roma151205@mail.ru>
2026-03-19 21:33:55 +0700

    set up groups display

---
+++ src/app/features/teacher/feature-journal-lessons/teacher-lessons-list/teacher-lessons-list.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'teacher-lessons-list',
+  imports: [],
+  templateUrl: './teacher-lessons-list.component.html',
+  styleUrl: './teacher-lessons-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TeacherLessonsListComponent {
+  private readonly route = inject(ActivatedRoute);
+
+  public readonly groupId = Number(this.route.snapshot.paramMap.get('groupId') ?? 0);
+}
