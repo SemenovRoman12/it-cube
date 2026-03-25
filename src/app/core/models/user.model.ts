7de045ebc4c208e5b1aed13dd14de38fa1dcd341
Qwayb <girdden@gmail.com>
2026-03-25 16:45:38 +0700

    base avatar features

--- src/app/core/models/user.model.ts
+++ src/app/core/models/user.model.ts
@@ -3,6 +3,7 @@ export interface UserEntity {
   full_name: string;
   email: string;
   password?: string;
+  avatar_url?: string | null;
   role: 'admin' | 'user' | 'teacher';
   group_id: number | null;
   created_at: string;
